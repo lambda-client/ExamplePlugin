1c318769bea2a41370629ccad4e74eb3d46c873b
Hudson <35442144+huddy987@users.noreply.github.com>
2021-04-30 21:20:46 -0600

    init

---
+++ src/main/kotlin/PluginTest.kt
@@ -0,0 +1,34 @@
+import com.lambda.client.module.Category
+import com.lambda.client.plugin.api.Plugin
+import com.lambda.client.plugin.api.PluginModule
+import com.lambda.client.util.text.MessageSendHelper
+
+internal class TestPlugin: Plugin() {
+
+    override fun onLoad() {
+        this.config
+        this.modules.add(TestModule())
+        MessageSendHelper.sendChatMessage("Hi")
+    }
+
+    override fun onUnload() {
+        MessageSendHelper.sendChatMessage("Bye")
+    }
+}
+
+internal class TestModule: PluginModule(
+    name = "TestModule",
+    category = Category.MISC,
+    description = "We do a little testing",
+    pluginMain = TestPlugin()
+) {
+    init {
+        onEnable {
+            MessageSendHelper.sendChatMessage("Hi :)")
+        }
+
+        onDisable {
+            MessageSendHelper.sendChatMessage("Bye :(")
+        }
+    }
+}
