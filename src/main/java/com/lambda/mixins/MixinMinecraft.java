14251c78d9ad94c968c3f5a6f14447a1d08730ae
Constructor <fractalminds@protonmail.com>
2022-02-18 06:15:46 +0100

    Update to latest plugin_info.json format.

--- src/main/java/com/lambda/mixins/MixinMinecraft.java
+++ src/main/java/com/lambda/mixins/MixinMinecraft.java
@@ -10,11 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinMinecraft {
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void clinit(CallbackInfo ci) {
-        System.out.println("THIS FUCKING WORKS\n\n\n\n\n\n\n\n\n\n##########################################");
-    }
-
-    @Inject(method = "run()V", at = @At("HEAD"))
-    public void run(CallbackInfo ci) {
-        System.out.println("This also works");
+        System.out.println("\n\n\n\n\n\n\n\nThis got called by minecraft and is your custom mixin.\n\n\n\n\n\n\n\n");
     }
 }
